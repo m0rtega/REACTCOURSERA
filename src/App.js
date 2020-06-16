3c317e9e28bdcd1185e26b8c1937112fffaecf26
Martin Ortega <martinortegayung@gmail.com>
2020-06-16 16:55:23 -0600

    Components Part 2

--- src/App.js
+++ src/App.js
@@ -1,19 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Navbar, NavbarBrand } from 'reactstrap';
 import './App.css';
 import Menu from './components/MenuComponent';
+import { DISHES } from './shared/dishes';
 
-const App = () => (
-  <div className="App">
-    <Navbar dark color="primary">
-      <div className="container">
-        <NavbarBrand href="/">
-          Whatever
-        </NavbarBrand>
-      </div>
-    </Navbar>
-    <Menu />
-  </div>
-)
+const App = () => {
+
+  const [dishes, setDishes] = useState(DISHES);
+
+  return(
+    <div className="App">
+      <Navbar dark color="primary">
+        <div className="container">
+          <NavbarBrand href="/">
+            Whatever
+          </NavbarBrand>
+        </div>
+      </Navbar>
+      <Menu dishes={dishes}/>
+    </div>
+  )
+}
 
 export default App;
